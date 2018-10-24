3d870729f379fb3940d7951365e930f0e7170fbc
colinthompson <colinthompson1111@gmail.com>
2018-10-24 14:12:25 -0600

    Initial Commit

---
+++ HalloweenDetector/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  HalloweenDetector
+//
+//  Created by Colin Thompson on 2018-10-23.
+//  Copyright © 2018 CampusMAppTeam. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ HalloweenDetector/VisionObjectRecognitionViewController.swift
@@ -0,0 +1,132 @@
+//
+//  VisionObjectRecognitionViewController.swift
+//  HalloweenDetector
+//
+//  Created by Colin Thompson on 2018-10-23.
+//  Copyright © 2018 CampusMAppTeam. All rights reserved.
+//  Based Upon: https://developer.apple.com/documentation/vision/recognizing_objects_in_live_capture
+
+import UIKit
+import AVFoundation
+import Vision
+
+class VisionObjectRecognitionViewController: ViewController {
+
+    private var detectionOverlay: CALayer! = nil
+    @IBOutlet weak var objectNameLabel: UILabel!
+    
+    // Vision parts
+    private var requests = [VNRequest]()
+    
+    @discardableResult
+    func setupVision() -> NSError? {
+        
+        self.view.bringSubviewToFront(objectNameLabel)
+        
+        // Setup Vision parts
+        let error: NSError! = nil
+        
+        do {
+            let visionModel = try VNCoreMLModel(for: HalloweenClassifier().model)
+            let objectRecognition = VNCoreMLRequest(model: visionModel, completionHandler: { (request, error) in
+                DispatchQueue.main.async(execute: {
+                    // perform all the UI updates on the main queue
+                    if let results = request.results {
+                        self.drawVisionRequestResults(results)
+                    }
+                })
+            })
+            self.requests = [objectRecognition]
+        } catch let error as NSError {
+            print("Model loading went wrong: \(error)")
+        }
+        
+        return error
+    }
+    
+    func drawVisionRequestResults(_ results: [Any]) {
+        CATransaction.begin()
+        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+        detectionOverlay.sublayers = nil // remove all the old recognized objects
+        
+        // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
+        let classifications = results as! [VNClassificationObservation]
+
+        if classifications.isEmpty {
+            print("Nothing recognized.")
+        } else {
+            // Display top classifications ranked by confidence in the UI.
+            let topClassification = classifications.first
+            if (topClassification?.confidence ?? 0 >= 0.99) {
+                objectNameLabel.text = topClassification?.identifier
+                objectNameLabel.isHidden = false
+            } else {
+                objectNameLabel.isHidden = true
+            }
+        }
+        
+        self.updateLayerGeometry()
+        CATransaction.commit()
+    }
+    
+    override func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        
+        let exifOrientation = exifOrientationFromDeviceOrientation()
+        
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: exifOrientation, options: [:])
+        do {
+            try imageRequestHandler.perform(self.requests)
+        } catch {
+            print(error)
+        }
+    }
+    
+    override func setupAVCapture() {
+        super.setupAVCapture()
+        
+        // setup Vision parts
+        setupLayers()
+        updateLayerGeometry()
+        setupVision()
+        
+        // start the capture
+        startCaptureSession()
+    }
+    
+    func setupLayers() {
+        detectionOverlay = CALayer() // container layer that has all the renderings of the observations
+        detectionOverlay.name = "DetectionOverlay"
+        detectionOverlay.bounds = CGRect(x: 0.0,
+                                         y: 0.0,
+                                         width: bufferSize.width,
+                                         height: bufferSize.height)
+        detectionOverlay.position = CGPoint(x: rootLayer.bounds.midX, y: rootLayer.bounds.midY)
+        rootLayer.addSublayer(detectionOverlay)
+    }
+    
+    func updateLayerGeometry() {
+        let bounds = rootLayer.bounds
+        var scale: CGFloat
+        
+        let xScale: CGFloat = bounds.size.width / bufferSize.height
+        let yScale: CGFloat = bounds.size.height / bufferSize.width
+        
+        scale = fmax(xScale, yScale)
+        if scale.isInfinite {
+            scale = 1.0
+        }
+        CATransaction.begin()
+        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+        
+        // rotate the layer into screen orientation and scale and mirror
+        detectionOverlay.setAffineTransform(CGAffineTransform(rotationAngle: CGFloat(.pi / 2.0)).scaledBy(x: scale, y: -scale))
+        // center the layer
+        detectionOverlay.position = CGPoint (x: bounds.midX, y: bounds.midY)
+        
+        CATransaction.commit()
+        
+    }
+}
